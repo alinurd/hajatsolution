bad433f479cb5e5247679ae1cbf4fcefc20b0f74
ali dev14 <ali.nrdn14005@gmail.com>
2024-05-04 03:34:14 +0700

    setup pembayaran minus invoice & detail pembayaran

--- public/application/views/admin/owner/list_booking.php
+++ public/application/views/admin/owner/list_booking.php
@@ -25,6 +25,8 @@
                                 <th>Nama Customer</th>
                                 <th>Nama Event</th>
                                 <th>Tanggal Acara</th>
+                                <th>Harga Rp.</th>
+                                <th>Masuk Rp.</th>
                                 <th>Status</th>
                                 <th class="max-width-120"><?php echo trans('options'); ?></th>
                             </tr>
@@ -40,6 +42,25 @@
                                     <td><?php echo html_escape($k->nama); ?></td>
                                     <td><?php echo html_escape($k->title); ?></td>
                                     <td><?php echo html_escape($k->tanggal_acara); ?></td>
+                                    <td class="pull-right"><b><?php echo $k->harga !== null && $k->harga !== 0 ? number_format($k->harga) : '0'; ?></b></td>
+
+                                    <td>
+                                     <?php
+                                    $this->db->where('id_booking', $k->id_booking);
+                                    $query = $this->db->get("bayar");
+                                    $byr= $query->result();
+                                    $tM = 0;
+                                    foreach ($byr as $item) {
+                                        $tM += $item->jumlah;
+                                    }
+                                   ?><b class="pull-right"><?php echo $tM !== null && $tM !== 0 ? number_format($tM) : '0'; ?></b><br>
+                                     <center>
+                                     ( <?=count($byr)?> x Bayar )
+                                    <br>
+                                <a href="">Detail</a>
+                                     </center>
+                            </td>
+
                                     <td>
                                         <?php if ($k->status_booking == 1) : ?>
                                             <label class="label bg-info    ">Pending</label>
@@ -51,82 +72,78 @@
                                     </td>
 
                                     <td>
-                                        <button type="button" class="btn-list-button" data-toggle="modal" data-target="#myModal<?= $k->id_booking ?>" onclick="$('#modal_user_id').val('<?php echo html_escape($k->id_booking); ?>');">
-                                            <i class="fa fa-user option-icon"></i>Update Pembayaran
+                                        <button type="button" class="btn bg-purple" data-toggle="modal" data-target="#myModal<?= $k->id_booking ?>" onclick="$('#modal_user_id').val('<?php echo html_escape($k->id_booking); ?>');">
+                                            <i class="glyphicon glyphicon-usd  option-icon"></i>Update Pembayaran
                                         </button>
                                         <?php echo form_open('owner_controller/booking_options_post'); ?>
                                         <input type="hidden" name="id" value="<?php echo html_escape($k->id_booking); ?>">
-
-                                        <div class="dropdown">
-                                            <button class="btn bg-purple dropdown-toggle btn-select-option" type="button" data-toggle="dropdown"><?php echo trans('select_option'); ?>
-                                                <span class="caret"></span>
-                                            </button>
-
-                                            <ul class="dropdown-menu options-dropdown">
-                                                <li>
-
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:void(0)" onclick="delete_item('admin_controller/delete_user_post','<?php echo $k->id_booking; ?>','<?php echo trans("confirm_user"); ?>');"><i class="fa fa-trash option-icon"></i><?php echo trans('delete'); ?></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-
                                         <?php echo form_close(); ?><!-- form end -->
                                     </td>
 
                                 </tr>
                                 <div id="myModal<?= $k->id_booking ?>" class="modal fade" role="dialog">
-                                    <div class="modal-dialog">
+    <div class="modal-dialog">
 
-                                        <!-- Modal content-->
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">Tenor Pembayaran [<?= $k->title ?> - <?= $k->nama ?>]</h4>
-                                            </div>
-                                            <?php echo form_open('owner_controller/update_pembayaran'); ?>
-                                            <input type="hidden" name="id_booking" id="modal_user_id" value="<?= $k->id_booking ?>">
-                                            <div class="modal-body">
-                                                <div class="form-group">
-                                                    <label for="jml" class="">jumlah Pembayaran </label>
-                                                    <input type="number" id="jml" name="jumlah" class="form-control">
-                                                </div>
-                                                <div class="form-group">
-										<label class="control-label">Tenor Pembayaran :</label>
-										<div class="input-group">
-											<span class="input-group-btn">
-												<button class="btn btn-primary btn-md" id="tenoskurang" type="button">-</button>
-											</span>
-											<input id="tenorInput" type="text" readonly class="form-control text-center col-md-6" name="tenor" placeholder="Tenor Pembayaran" value="1">
-											<span class="input-group-btn">
-												<button class="btn btn-primary btn-md" id="tenortambah" type="button">+</button>
-											</span>
-										</div>
-									</div>
-                                                <!-- <div class="form-group">
-                                                    <div class="row">
-                                                        <?php
-                                                        for ($i = 1; $i <= $k->tenor; $i++) { ?>
-                                                            <div class="col-sm-4 col-xs-12 col-option">
-                                                                <input type="radio" id="tenor<?= $i ?>" name="tenor" value="<?= $i ?>" class="square-purple" <?= $i === 1 ? 'checked' : '' ?>>
-                                                                <label for="tenor<?= $i ?>" class="cursor-pointer">Pembayaran ke <?= $i ?></label>
-                                                            </div>
-                                                        <?php } ?>
-                                                    </div>
-                                                </div> -->
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Tenor Pembayaran [<?= $k->title ?> - <?= $k->nama ?>]</h4>
+            </div>
+            <?php echo form_open('owner_controller/update_pembayaran'); ?>
+            <input type="hidden" name="id_booking" id="modal_user_id" value="<?= $k->id_booking ?>">
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="jml" class="">jumlah Pembayaran </label>
+                    <input type="number" id="jml" name="jumlah" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Tenor Pembayaran :</label>
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary btn-md" id="tenoskurang<?= $k->id_booking ?>" type="button">-</button>
+                        </span>
+                        <input type="hidden" id="countTenor<?= $k->id_booking ?>" value="<?php echo count($byr) > 0 ? count($byr) : '1'; ?>">
+                        <input id="tenorInput<?= $k->id_booking ?>" type="text" readonly class="form-control text-center col-md-6" name="tenor" placeholder="Tenor Pembayaran" value="<?php echo count($byr) > 0 ? count($byr) : '0'; ?>">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary btn-md" id="tenortambah<?= $k->id_booking ?>" type="button">+</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success"><?php echo trans('save'); ?></button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo trans('close'); ?></button>
+                </div>
+            </div>
+            <?php echo form_close(); ?>
+            <!-- form end -->
+        </div>
+    </div>
+</div>
 
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="submit" class="btn btn-success"><?php echo trans('save'); ?></button>
-                                                <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo trans('close'); ?></button>
-                                            </div>
+<script>
+    $(document).ready(function() {
+        var value = parseInt($('#tenorInput<?= $k->id_booking ?>').val()) || 0;
+            var tenor = parseInt($('#countTenor<?= $k->id_booking ?>').val()) || 0;
+            // alert(value)
+            $('#tenorInput<?= $k->id_booking ?>').val(value + 1);
+            
+        // $('#tenortambah<?= $k->id_booking ?>').click(function() {
+        //     var value = parseInt($('#tenorInput<?= $k->id_booking ?>').val()) || 0;
+        //     $('#tenorInput<?= $k->id_booking ?>').val(value + 1);
+        // });
 
-                                            <?php echo form_close(); ?><!-- form end -->
-                                        </div>
+        // $('#tenoskurang<?= $k->id_booking ?>').click(function() {
+        //     if (value > tenor) {
+        //         $('#tenorInput<?= $k->id_booking ?>').val(value - 1);
+        //     } else {
+        //         alert("Tenor anda sudah membayar "+tenor+" kali Pembayaran"+" silahkan sesuaikan tenor Pembayaran");
+        //     }
+        // });
 
-                                    </div>
-                                </div>
+    })
+</script>
+
                             <?php endforeach; ?>
 
                         </tbody>
@@ -137,21 +154,3 @@
     </div><!-- /.box-body -->
 </div>
 
-
-<script>
-	$(document).ready(function() {
-		$('#tenortambah').click(function() {
-			var value = parseInt($('#tenorInput').val()) || 0;
-			$('#tenorInput').val(value + 1);
-		});
-
-		$('#tenoskurang').click(function() {
-			var value = parseInt($('#tenorInput').val()) || 0;
-			if (value > 1) {
-				$('#tenorInput').val(value - 1);
-			} else {
-				alert("Tenor Pembayaran Minimal 1 Kali")
-			}
-		});
-	});
-</script>
